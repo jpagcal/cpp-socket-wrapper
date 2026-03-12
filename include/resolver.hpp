a8c0ab844425d4bacb6f2dcf3d8f7fdc1c3ae9b9
jpagcal <jpagcal_@outlook.com>
2026-03-12 12:36:46 -0600

    docs: resolver

--- include/resolver.hpp
+++ include/resolver.hpp
@@ -10,28 +10,76 @@
 
 namespace conn_resolver {
 
+/*
+ * @brief A structure to represent the hint parameters passed into getaddrinfo
+ */
 struct ResolverHints {
-	int32_t endpoint_type = networking::socket_type::raw;
-	int32_t ip_domain = networking::domain::unspecified_domain;
-	int32_t protocol;
-	int32_t flags;
+	int32_t endpoint_type = networking::socket_type::raw; /**< The socket type of the current endpoint */
+	int32_t ip_domain = networking::domain::unspecified_domain; /**< The IP domain that the resolver should search */
+	int32_t protocol; /**< Limits the returned address structure to a specific protocol */
+	int32_t flags; /**< Additional flags. Check 'man getaddrinfo' */
 };
 
+/*
+ * @brief Creates the raw struct addrinfo out of struct ResolverHints
+ *
+ * @param hints The resolver hints struct
+ */
 addrinfo craft_resolver_hints(ResolverHints &hints);
 
+/*
+ * @brief The Resolver class
+ *
+ * Encapsulates getaddrinfo functionality, providing methods to filter, print, and connect to results
+ */
 class Resolver {
 	using QueryResults = std::vector<AddressInfo>;
 	using RawResults = addrinfo *;
 public:
+
+	/*
+	 * @brief The Resolver constructor
+	 *
+	 * @param hostname the host to connect to
+	 * @param service the desired service of the target host
+	 */
 	Resolver(std::string hostname, std::string service);
+
+	/*
+	 * @brief Returns attribute query_results_
+	 */
 	QueryResults results() const;
+
+	/*
+	 * @brief Returns only UDP nodes in query_results_
+	 */
 	QueryResults udp_nodes() const;
+
+	/*
+	 * @brief Returns only TCP nodes in query_results
+	 */
 	QueryResults tcp_nodes() const;
+
+	/*
+	 * @brief Attempts to connect to the target host using protocol TCP
+	 *
+	 * @returns a TCP connection to the target host
+	 */
 	tcp::Connection try_connect_tcp();
+
+	/*
+	* @brief Attempts to connect to the target host using protocol UDP
+	*
+	* @returns a UDP connection to the target host
+	*/
 	void try_connect_udp(); // TODO: create UDPConnection class
+
+	/*
+	 * @brief prints query results to stdout
+	 */
 	void print_results() const;
 
 private:
-	QueryResults query_results_;
+	QueryResults query_results_; /**< A vector of results filled by the constructor */
 };
 } // namespace resolver
