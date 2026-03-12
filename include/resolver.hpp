b78f85189918e4079ae22e16d8d9000c85d96772
jpagcal <jpagcal_@outlook.com>
2026-03-12 12:43:31 -0600

    fix documentation to include javadoc-style markers

--- include/resolver.hpp
+++ include/resolver.hpp
@@ -10,7 +10,7 @@
 
 namespace conn_resolver {
 
-/*
+/**
  * @brief A structure to represent the hint parameters passed into getaddrinfo
  */
 struct ResolverHints {
@@ -20,14 +20,14 @@ struct ResolverHints {
 	int32_t flags; /**< Additional flags. Check 'man getaddrinfo' */
 };
 
-/*
+/**
  * @brief Creates the raw struct addrinfo out of struct ResolverHints
  *
  * @param hints The resolver hints struct
  */
 addrinfo craft_resolver_hints(ResolverHints &hints);
 
-/*
+/**
  * @brief The Resolver class
  *
  * Encapsulates getaddrinfo functionality, providing methods to filter, print, and connect to results
@@ -37,7 +37,7 @@ class Resolver {
 	using RawResults = addrinfo *;
 public:
 
-	/*
+	/**
 	 * @brief The Resolver constructor
 	 *
 	 * @param hostname the host to connect to
@@ -45,36 +45,36 @@ public:
 	 */
 	Resolver(std::string hostname, std::string service);
 
-	/*
+	/**
 	 * @brief Returns attribute query_results_
 	 */
 	QueryResults results() const;
 
-	/*
+	/**
 	 * @brief Returns only UDP nodes in query_results_
 	 */
 	QueryResults udp_nodes() const;
 
-	/*
+	/**
 	 * @brief Returns only TCP nodes in query_results
 	 */
 	QueryResults tcp_nodes() const;
 
-	/*
+	/**
 	 * @brief Attempts to connect to the target host using protocol TCP
 	 *
 	 * @returns a TCP connection to the target host
 	 */
 	tcp::Connection try_connect_tcp();
 
-	/*
+	/**
 	* @brief Attempts to connect to the target host using protocol UDP
 	*
 	* @returns a UDP connection to the target host
 	*/
 	void try_connect_udp(); // TODO: create UDPConnection class
 
-	/*
+	/**
 	 * @brief prints query results to stdout
 	 */
 	void print_results() const;
